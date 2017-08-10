e981b3795857188800e491552c68b4235b452f1a
Ritvik Annam <ritu6ritu@gmail.com>
2017-08-10 17:11:27 -0500

    Basic parsing

---
+++ index.js
@@ -0,0 +1,45 @@
+var fb = require("facebook-chat-api");
+var db = require("mongodb");
+
+var my_id = 1;
+
+fb({email: "EMAIL", password: "PASSWORD"}, (err, api) => {
+  api.listen((err, event) => {
+    if(event.type === "message"){
+      var body = event.body.trim();
+      var words = body.split(/\s+/g);
+      if(words[0] === '/gmbot' || words[0] === '@gmbot'){
+
+      }
+    }
+  });
+});
+
+
+function parseCommand(words, message){
+  if (!words[1]) return help("Invalid Command - try one of these:");
+
+  switch words[1].toLowerCase(){
+    case "make":
+      makeGroup(words, message);
+      break;
+    case "delete":
+      deleteGroup(words, message);
+      break;
+    case "add":
+      addToGroup(words, message);
+      break;
+    case "remove":
+      removeFromGroup(words, message);
+      break;
+    case "rename":
+      renameGroup(words, message);
+      break;
+    case "help":
+      help();
+      break;
+    default:
+      help("Invalid Command - try one of these:");
+      break;
+  }
+}
